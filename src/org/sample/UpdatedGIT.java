afbb909632c52517e1ef84930bc2bc173be5b022
sumathikondala <sumathi.kondala@gmail.com>
2024-05-22 00:57:57 +0530

    Done my work

---
+++ src/org/sample/UpdatedGIT.java
@@ -0,0 +1,19 @@
+package org.sample;
+
+public class UpdatedGIT {
+		private void newMethod() {
+			String s  = "Welcome to My World";
+			System.out.println(s);
+			
+			int a =10;
+			System.out.println(a);
+			}
+
+		public static void main(String[] args) {
+						
+			UpdatedGIT m1 = new UpdatedGIT();
+			m1.newMethod();
+		
+	}
+
+}
