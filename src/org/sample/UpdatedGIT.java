4a83a0d66bc76913f3a821c9ee272d686eaf4921
sumathikondala <sumathi.kondala@gmail.com>
2024-05-22 12:37:48 +0530

    Arul code merge

--- src/org/sample/UpdatedGIT.java
+++ src/org/sample/UpdatedGIT.java
@@ -8,14 +8,14 @@ public class UpdatedGIT {
 			int a =10;
 			System.out.println(a);
 			
-<<<<<<< HEAD
-			double d = 23.543;
-			System.out.println(d);
-=======
 			float f = 38.45f;
 			System.out.println(f);
+
 			
->>>>>>> 70c7f33daa376cc0c45caadd16d60843d9c97202
+			double d = 23.543;
+			System.out.println(d);
+				
+
 			}
 
 		public static void main(String[] args) {
